513278c16f50cbbebc56f93df56ab811d8bb2685
Aceslup <aceslup@users.noreply.github.com>
2021-07-28 21:45:45 +0800

    Add openwrt-xray, luci-app-xray

--- diy-plugins-lean.sh
+++ diy-plugins-lean.sh
@@ -88,4 +88,13 @@ rm -rf openwrt-package
 #mv luci-theme-argon lua-maxminddb luci-app-vssr `pwd`/package/lean/
 
 
+# Add openwrt-xray, luci-app-xray
+git clone https://github.com/yichya/openwrt-xray
+git clone https://github.com/yichya/luci-app-xray
+LUCI_NET_PATH="`pwd`/feeds/packages/net"
+LUCI_APP_PATH="`pwd`/feeds/luci/applications"
+mv openwrt-xray ${LUCI_NET_PATH}
+mv luci-app-xray ${LUCI_APP_PATH}
+
+
 ./scripts/feeds update -a
