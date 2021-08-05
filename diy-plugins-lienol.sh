29cd3ddf4b52efb09b39aaf8db172dd0c1400eff
Aceslup <aceslup@users.noreply.github.com>
2021-08-05 11:30:48 +0800

    Update diy-plugins-lienol.sh

--- diy-plugins-lienol.sh
+++ diy-plugins-lienol.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
 #
 
-# Add SmartDNS
-WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
-unzip $WORKINGDIR/master.zip -d $WORKINGDIR
-mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
-rmdir $WORKINGDIR/openwrt-smartdns-master
-rm $WORKINGDIR/master.zip
+## Add SmartDNS
+#WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+#mkdir $WORKINGDIR -p
+#rm $WORKINGDIR/* -fr
+#wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+#unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+#mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+#rmdir $WORKINGDIR/openwrt-smartdns-master
+#rm $WORKINGDIR/master.zip
+#
+## master分支  为openwrt 19.07之后版本使用, 此版本基于javascript
+## lede分支    为lede分支使用, 此版本基于lua
+#LUCIBRANCH="lede"
+#
+#WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+#mkdir $WORKINGDIR -p
+#rm $WORKINGDIR/* -fr
+#wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
+#unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
+#mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
+#rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
+#rm $WORKINGDIR/${LUCIBRANCH}.zip
 
-# master分支  为openwrt 19.07之后版本使用, 此版本基于javascript
-# lede分支    为lede分支使用, 此版本基于lua
-LUCIBRANCH="lede"
 
-WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
-mkdir $WORKINGDIR -p
-rm $WORKINGDIR/* -fr
-wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
-unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
-mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
-rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
-rm $WORKINGDIR/${LUCIBRANCH}.zip
-
-
-# Add PassWall
+## Add PassWall
 #git clone https://github.com/xiaorouji/openwrt-passwall.git
 #mv openwrt-passwall/* `pwd`/package/lean/
 #rm -rf openwrt-passwall
 
 
-# Add Kcptun
+## Add Kcptun
 #git clone https://github.com/kuoruan/luci-app-kcptun.git `pwd`/package/luci-app-kcptun
 
 
-# Add luci-app-jd-dailybonus
+## Add luci-app-jd-dailybonus
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git `pwd`/package/lean/luci-app-jd-dailybonus
 
 
-# Add SRP+ dept
+## Add SRP+ dept
 #git clone https://github.com/coolsnowwolf/lede
 #cp -r lede/package/lean/{tcpping,microsocks,redsocks2} feeds/helloworld/
 #rm -rf lede
 
 
-# Add ServerChan
+## Add ServerChan
 #git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng
 #cp -r feeds-xiaoqingfeng/luci-app-serverchan `pwd`/package/lean/
 #rm -rf feeds-xiaoqingfeng
 
 
-# Add homeclash
+## Add homeclash
 #git clone https://github.com/xiaoqingfengATGH/homeclash
 #cp -r homeclash/luci-app-openclash `pwd`/package/lean/
 #rm -rf homeclash
@@ -84,9 +84,9 @@ mv ${GITCLONE_APP}/luci-app-ttnode ${LUCI_APP_PATH}/
 rm -rf openwrt-package
 
 
-# From kenzok8/openwrt-packages
+## From kenzok8/openwrt-packages
 #git clone https://github.com/kenzok8/openwrt-packages
-# Add luci-app-jd-dailybonus
+## Add luci-app-jd-dailybonus
 #cp -r openwrt-packages/luci-app-jd-dailybonus `pwd`/package/lean/
 ## Add luci-app-gost
 #cp -r openwrt-packages/{luci-app-gost,gost} `pwd`/package/lean/
@@ -97,7 +97,7 @@ rm -rf openwrt-package
 #rm -rf openwrt-packages
 
 
-# Add HelloWorld, lua-maxminddb dep+
+## Add HelloWorld, lua-maxminddb dep+
 #rm -rf `pwd`/package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 #git clone https://github.com/jerrykuku/lua-maxminddb.git
