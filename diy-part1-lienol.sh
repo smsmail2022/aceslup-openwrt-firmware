99ccc9d3d5d6f67de7acb07c37dc76769027826e
Aceslup <aceslup@users.noreply.github.com>
2021-08-06 15:24:27 +0800

    Update diy-part1-lienol.sh

--- diy-part1-lienol.sh
+++ diy-part1-lienol.sh
@@ -8,12 +8,17 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# Uncomment a feed source
+# Add 3rd packages lean: ssr+
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol-package https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add 3rd packages lienol: passwall,syncthing
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
+# Add 3rd packages kenzok8: passwall,ssr+,vssr,gost,smartdns,serverchan,jd-dailybonus
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+# Add 3rd packages homelede: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
+#sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' feeds.conf.default
