559726415ba60433c08df7f8640a60b59c1f989c
Aceslup <aceslup@users.noreply.github.com>
2021-08-09 00:45:05 +0800

    Update diy-part1-lienol.sh

--- diy-part1-lienol.sh
+++ diy-part1-lienol.sh
@@ -17,8 +17,8 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 # Add 3rd packages kenzok8: passwall,ssr+,vssr,gost,smartdns,serverchan,jd-dailybonus
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add 3rd packages homelede: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
 sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' feeds.conf.default
