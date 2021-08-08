c926b045e56a4dad270a4e8394a2a807163574a3
Aceslup <aceslup@users.noreply.github.com>
2021-08-09 00:07:12 +0800

    Update diy-part1-lienol.sh

--- diy-part1-lienol.sh
+++ diy-part1-lienol.sh
@@ -10,7 +10,7 @@
 
 # Add 3rd packages lean: ssr+
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 
 # Add 3rd packages lienol: passwall,syncthing
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
@@ -21,4 +21,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add 3rd packages homelede: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
-#sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' feeds.conf.default
+sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' feeds.conf.default
+
+# Add 3rd packages vernesong: openclash
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
