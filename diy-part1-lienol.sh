eef73948602cf49177d2b4edf988042162034a76
Aceslup <aceslup@users.noreply.github.com>
2022-04-18 14:54:59 +0800

    Update diy-part1-lienol.sh

--- diy-part1-lienol.sh
+++ diy-part1-lienol.sh
@@ -8,27 +8,136 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# Add 3rd packages lean: ssr+
+#【lean】: ssr+
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+#sed -i '$a src-git lean https://github.com/coolsnowwolf/packages' feeds.conf.default
 
-# Add 3rd packages lienol: passwall,syncthing
+
+#【lienol】: passwall,syncthing
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
-# Add 3rd packages kenzok8: passwall,ssr+,vssr,gost,smartdns,serverchan,jd-dailybonus
+
+#【kenzok8】: passwall,ssr+,vssr,gost,smartdns,serverchan,jd-dailybonus
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# 追新软件包
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
-# Add 3rd packages homelede: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
+
+#【xiaorouji】:
+#sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+
+#【homelede】: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
 #sed -i '$a src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng' feeds.conf.default
 
-# Add 3rd packages vernesong: openclash
+
+#【vernesong】: openclash
 #sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
 
-# Add 3rd packages nxhack: node
+
+#【nxhack】: node
 #sed -i '$a src-git node https://github.com/nxhack/openwrt-node-packages.git' feeds.conf.default
 
-# Add 3rd packages lean: 
-#sed -i '$a src-git lean https://github.com/coolsnowwolf/packages' feeds.conf.default
+
+
+######################################################## 散装软件
+#【pymumu】：SmartDNS
+#WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
+#mkdir $WORKINGDIR -p
+#rm $WORKINGDIR/* -fr
+#wget https://github.com/pymumu/openwrt-smartdns/archive/master.zip -O $WORKINGDIR/master.zip
+#unzip $WORKINGDIR/master.zip -d $WORKINGDIR
+#mv $WORKINGDIR/openwrt-smartdns-master/* $WORKINGDIR/
+#rmdir $WORKINGDIR/openwrt-smartdns-master
+#rm $WORKINGDIR/master.zip
+
+## master分支  为openwrt 19.07之后版本使用, 此版本基于javascript
+## lede分支    为lede分支使用, 此版本基于lua
+#LUCIBRANCH="lede"
+
+#WORKINGDIR="`pwd`/feeds/luci/applications/luci-app-smartdns"
+#mkdir $WORKINGDIR -p
+#rm $WORKINGDIR/* -fr
+#wget https://github.com/pymumu/luci-app-smartdns/archive/${LUCIBRANCH}.zip -O $WORKINGDIR/${LUCIBRANCH}.zip
+#unzip $WORKINGDIR/${LUCIBRANCH}.zip -d $WORKINGDIR
+#mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
+#rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
+#rm $WORKINGDIR/${LUCIBRANCH}.zip
+
+
+#【kuoruan】：Kcptun
+#git clone https://github.com/kuoruan/luci-app-kcptun.git `pwd`/package/luci-app-kcptun
+
+
+#【xiaoqingfengATGH】：ServerChan
+#git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng
+#cp -r feeds-xiaoqingfeng/luci-app-serverchan `pwd`/package/lean/
+#rm -rf feeds-xiaoqingfeng
+
+
+#【281677160】: clash,openclash,vssr,gost,gowebdav,pushbot,serverchan,smartdns,syncthing,ttnode,oaf
+#git clone https://github.com/281677160/openwrt-package
+#GITCLONE_APP="openwrt-package/feeds/luci/applications"
+#GITCLONE_NET="openwrt-package/feeds/packages/net"
+#GITCLONE_UTILS="openwrt-package/feeds/packages/utils"
+
+#LUCI_APP_PATH="`pwd`/feeds/luci/applications" && mkdir -pv ${LUCI_APP_PATH}
+#LUCI_NET_PATH="`pwd`/feeds/packages/net" && mkdir -pv ${LUCI_NET_PATH}
+#LUCI_UTILS_PATH="`pwd`/feeds/packages/utils" && mkdir -pv ${LUCI_UTILS_PATH}
+
+## Add gost
+#mv ${GITCLONE_APP}/luci-app-gost ${LUCI_APP_PATH}/
+#mv ${GITCLONE_NET}/gost ${LUCI_NET_PATH}/
+## Add gowebdav
+#mv ${GITCLONE_APP}/luci-app-gowebdav ${LUCI_APP_PATH}/
+#mv ${GITCLONE_NET}/gowebdav ${LUCI_NET_PATH}/
+## Add luci-app-syncthing
+#mv ${GITCLONE_APP}/luci-app-syncthing ${LUCI_APP_PATH}/
+## Add luci-app-pushbot
+#mv ${GITCLONE_APP}/luci-app-pushbot ${LUCI_APP_PATH}/
+## Add luci-app-serverchan
+#mv ${GITCLONE_APP}/luci-app-serverchan ${LUCI_APP_PATH}/
+## Add luci-app-ttnode
+#mv ${GITCLONE_APP}/luci-app-ttnode ${LUCI_APP_PATH}/
+#rm -rf openwrt-package
+
+
+#【kenzok8】: passwall,ssr+,vssr,gost,smartdns,serverchan,jd-dailybonus
+#git clone https://github.com/kenzok8/openwrt-packages
+# Add luci-app-gost
+#cp -r openwrt-packages/{luci-app-gost,gost} `pwd`/package/lean/
+# Add luci-app-openclash
+#cp -r openwrt-packages/luci-app-openclash `pwd`/package/lean/
+# Add luci-app-clash
+#cp -r openwrt-packages/luci-app-clash `pwd`/package/lean/
+#rm -rf openwrt-packages
+
+
+#【jerrykuku】：HelloWorld, lua-maxminddb dep+
+#rm -rf `pwd`/package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
+#git clone https://github.com/jerrykuku/luci-app-vssr.git 
+#mv luci-theme-argon lua-maxminddb luci-app-vssr `pwd`/package/lean/
+
+
+#【xiaoqingfengATGH】: homeconnect,ssr+,smartdns,serverchan,jd-dailybonus
+# Add luci-app-homeconnect
+#git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git
+#mv feeds-xiaoqingfeng/luci-app-homeconnect `pwd`/package/lean/
+#mv feeds-xiaoqingfeng/softethervpn5 `pwd`/package/lean/
+#rm -rf feeds-xiaoqingfeng
+
+
+#【xiaoqingfengATGH】: homeclash
+# Add homeclash
+#git clone https://github.com/xiaoqingfengATGH/homeclash
+#cp -r homeclash/luci-app-openclash `pwd`/package/lean/
+#rm -rf homeclash
+
+
+#【jerrykuku】：luci-app-jd-dailybonus
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git `pwd`/package/lean/luci-app-jd-dailybonus
